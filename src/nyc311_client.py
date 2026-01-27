4ba9545b33f8319b669646f20381533a4370230e
Sean Huvaya <sean.ktxdev@gmail.com>
2026-01-27 11:55:06 -0500

    Implement fetch complaints

---
+++ src/nyc311_client.py
@@ -0,0 +1,31 @@
+from src.base_client import BaseAPIClient
+
+
+class NYC311_Client(BaseAPIClient):
+    def __init__(self, app_token: str):
+        super().__init__(
+            base_url="https://data.cityofnewyork.us/resource/",
+            headers={
+                "X-App-Token": app_token,
+                "Accept": "application/json"
+            }
+        )
+
+    def get_complaints(
+            self,
+            start_date: str,
+            end_date: str,
+            limit: int = 1000,
+            offset: int = 0
+    ):
+        params = {
+            "$select": ("unique_key,created_date,closed_date,agency,agency_name,complaint_type,descriptor,",
+                        "incident_zip,borough,status,open_data_channel_type,latitude,longitude"),
+            "$where": f"created_date BETWEEN '{start_date}' AND '{end_date}'",
+            "$order": "created_date ASC",
+            "$limit": limit,
+            "$offset": offset
+
+        }
+
+        return self.get("erm2-nwe9", params=params)
